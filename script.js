9729890683ec5ff79dc8016de0e6d665db4237b8
MakeCLIGreatAgain <ooarai1337@gmail.com>
2025-03-12 19:27:14 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,1 @@
+while(document.body.firstChild)document.body.firstChild.remove();document.title="ВЕБСАЙТ ИМЕНИ ЛЕССИ";i=new Image;i.src="https://i.imgur.com/hCMjyOL.jpeg";i.style='position:fixed;top:0;left:0;width:100%;height:100%;object-fit:contain';document.body.appendChild(i)
